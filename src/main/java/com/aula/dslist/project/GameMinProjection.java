77476c771a979e3a08bcfd8c6083db0987088c80
DiogoYudi <diogoyukui23@gmail.com>
2025-01-22 10:06:52 -0300

    Homolog ok

--- src/main/java/com/aula/dslist/project/GameMinProjection.java
+++ src/main/java/com/aula/dslist/project/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.aula.dslist.project;
 public interface GameMinProjection {
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
